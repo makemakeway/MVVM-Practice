f22dc5750411004a1fdf616b446d43024fedadbf
YeonBae Park <makemakeway@gmail.com>
2021-12-27 10:01:50 +0900

    Initial Commit

---
+++ SeSACWeek14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACWeek14
+//
+//  Created by 박연배 on 2021/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
